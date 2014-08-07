94886dda4e71db60f55a27304b6dc82e34f99ef9
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2014-08-07 10:35:57 +0100

    adding initial lein/compojure structure

---
+++ README.md
@@ -0,0 +1,19 @@
+# clojure-web-db-app
+
+A sample Web application using Compojure that talks to a mysql db (using Korma)
+
+## Prerequisites
+
+You will need [Leiningen][1] 1.7.0 or above installed.
+
+[1]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2014 FIXME
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject clojure-web-db-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler clojure-web-db-app.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]
+                        [korma "0.3.0"]
+                        [mysql/mysql-connector-java "5.1.25"]]}})
